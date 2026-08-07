010aad21e8b3f195214e316d70b746f79c0cab68
Luc Forget <dev@alias.lforget.fr>
2026-08-07 18:23:05 +0900

    Refactor to avoid duplicate sequence type

--- templeano/include/templeano/utils.hpp
+++ templeano/include/templeano/utils.hpp
@@ -3,18 +3,21 @@
 #include <type_traits>
 
 namespace templeano::utils {
-template <typename... SeqTypes> struct Seq;
+namespace detail {
 
-template <> struct Seq<> {
+template <typename... SeqTypes> struct SeqSpec;
+
+template <> struct SeqSpec<> {
   template <typename Predicate> using get_type_with_property = void;
 };
 
 template <typename SeqHead, typename... SeqTypes>
-struct Seq<SeqHead, SeqTypes...> {
+struct SeqSpec<SeqHead, SeqTypes...> {
   template <typename Predicate>
-  using get_type_with_property = std::conditional_t<
-      Predicate::template CheckFor<SeqHead>::value, SeqHead,
-      typename Seq<SeqTypes...>::template get_type_with_property<Predicate>>;
+  using get_type_with_property =
+      std::conditional_t<Predicate::template CheckFor<SeqHead>::value, SeqHead,
+                         typename SeqSpec<SeqTypes...>::
+                             template get_type_with_property<Predicate>>;
   using Head = SeqHead;
   static constexpr Head head{};
   template <typename Predicate>
@@ -23,11 +26,23 @@ struct Seq<SeqHead, SeqTypes...> {
     return {};
   }
 };
+} // namespace detail
 
-template <typename... SeqL, typename... SeqR>
-constexpr Seq<SeqL..., SeqR...> concat(Seq<SeqL...>, Seq<SeqR...>) {
-  return {};
-}
+template <typename... SeqTypes>
+struct Seq : public detail::SeqSpec<SeqTypes...> {
+  template <typename T>
+  static constexpr auto prepend(T) -> Seq<T, SeqTypes...> {
+    return {};
+  }
+  template <typename T> static constexpr auto append(T) -> Seq<SeqTypes..., T> {
+    return {};
+  }
+  template <typename... OtherSeqT>
+  static constexpr auto concat(Seq<OtherSeqT...>)
+      -> Seq<SeqTypes..., OtherSeqT...> {
+    return {};
+  }
+};
 
 } // namespace templeano::utils
 
